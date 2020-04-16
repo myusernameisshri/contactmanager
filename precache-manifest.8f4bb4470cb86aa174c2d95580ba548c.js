982cc8c6e756126249f8a0311d43a899598a5afe
Shrirang <shrirangkarpe1298@gmail.com>
2020-04-16 20:00:31 +0530

    Updates

---
+++ precache-manifest.8f4bb4470cb86aa174c2d95580ba548c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d81c8739b827657fe41c0ebdb27418",
+    "url": "/myusernameisshri/contactmanager/index.html"
+  },
+  {
+    "revision": "961b8782aa943f0307ea",
+    "url": "/myusernameisshri/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b3974ca8edc7005bf895",
+    "url": "/myusernameisshri/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "961b8782aa943f0307ea",
+    "url": "/myusernameisshri/contactmanager/static/js/2.f0812080.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/myusernameisshri/contactmanager/static/js/2.f0812080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3974ca8edc7005bf895",
+    "url": "/myusernameisshri/contactmanager/static/js/main.2a6c27d6.chunk.js"
+  },
+  {
+    "revision": "bd2e64b211c33499cb0e",
+    "url": "/myusernameisshri/contactmanager/static/js/runtime-main.a1f31958.js"
+  }
+]);
